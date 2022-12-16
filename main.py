c86fddf9d614d54f8f916ee80a1078c21426a567
Diego Alejandro Lozano Millan <diegoalejandrolozano@hotmail.es>
2022-12-16 17:58:38 -0500

    tweet_post

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ class Tweet(BaseModel):
         max_length=256
     )
     create_at:datetime = Field(default=datetime.now())
-    update_at:Optional[datetime] = Field(default=None)
+    updated_at:Optional[datetime] = Field(default=None)
     by:User = Field(...)
 
 #Path operations
@@ -97,10 +97,10 @@ def signup(user:UserRegister=Body(...)):
     """
     with open("users.json", "r+", encoding="utf-8") as f:
         results =json.loads(f.read())
-        user_dict = user.dict()
-        user_dict["user_id"] = str(user_dict["user_id"])
-        user_dict["birth_date"] = str(user_dict["birth_date"])
-        results.append(user_dict)
+        tweet_dict = user.dict()
+        tweet_dict["user_id"] = str(tweet_dict["user_id"])
+        tweet_dict["birth_date"] = str(tweet_dict["birth_date"])
+        results.append(tweet_dict)
         f.seek(0)
         f.write(json.dumps(results))
         return user
@@ -200,8 +200,37 @@ def home():
     summary="Post a tweet",
     tags=["Tweets"]
 )
-def post():
-    pass
+def post(tweet:Tweet=Body(...)):
+    """
+    Post a tweet
+
+    This path operation post a tweet in the app
+
+    Parameters:
+        -Request body parameter
+            -tweet: Tweet
+
+    Returns a json with the basic tweet information:
+        -tweet_id:UUID
+        -content:str
+        -create_at:datetime
+        -updated_at:Optional[datetime]
+        -by: User
+    """
+    with open("tweets.json", "r+", encoding="utf-8") as f:
+        results =json.loads(f.read())
+        tweet_dict = tweet.dict()
+        tweet_dict["tweet_id"] = str(tweet_dict["tweet_id"])
+        tweet_dict["create_at"] = str(tweet_dict["create_at"])
+        tweet_dict["updated_at"] = str(tweet_dict["updated_at"])
+        tweet_dict["by"]["user_id"] = str(tweet_dict["by"]["user_id"])
+        tweet_dict["by"]["birth_date"] = str(tweet_dict["by"]["birth_date"])
+
+
+        results.append(tweet_dict)
+        f.seek(0)
+        f.write(json.dumps(results))
+        return tweet
 
 ### Show a Tweet
 @app.get(
