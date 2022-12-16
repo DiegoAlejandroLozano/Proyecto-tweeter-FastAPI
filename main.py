1260dafc88fbbea4be499864b87307b16ab7224a
Diego Alejandro Lozano Millan <diegoalejandrolozano@hotmail.es>
2022-12-15 19:28:19 -0500

    users_signup

--- main.py
+++ main.py
@@ -40,6 +40,13 @@ class User(UserBase):
     )
     birth_date:Optional[date] = Field(default=None)
 
+class UserRegister(User):
+    password:str = Field(
+        ...,
+        min_length=8,
+        max_length=64
+    )
+
 class Tweet(BaseModel):
     tweet_id:UUID = Field(...)
     content:str = Field(
@@ -70,7 +77,22 @@ def home():
     tags=["User"]
 )
 def signup():
-    pass
+    """
+    Signup
+
+    This path operation register a user in the app
+
+    Parameters:
+        -Request body parameter
+            -user:UserRegister
+
+    Returns a json with the basic user information:
+        -user_id:UUID
+        -email:EmailStr
+        -first_name:str
+        -last_name:str
+        -birth_date:str
+    """
 
 ### Login a user
 @app.post(
